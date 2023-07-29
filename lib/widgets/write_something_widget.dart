084c2790d3ed83e25716abfe389a8bc03437d293
Moon-light2602 <dyndyn2602@gmail.com>
2023-07-29 16:34:32 +0700

    edit write something widget

--- lib/widgets/write_something_widget.dart
+++ lib/widgets/write_something_widget.dart
@@ -10,75 +10,48 @@ class WriteSomethingWidget extends StatelessWidget {
         children: <Widget>[
           Container(
             padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-            child: Row(
+            child: const Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
-                const CircleAvatar(
+                CircleAvatar(
                   radius: 22.0,
                   backgroundImage: AssetImage('assets/story5.jpg'),
                 ),
 
-                const SizedBox(width: 5.0),
-
-                Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 5.0, vertical: 10.0),
-                  height: 40.0,
-                  width: MediaQuery.of(context).size.width/1.4,
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      width: 1.0,
-                      color: Colors.white54
+                SizedBox(width: 5.0),
+                
+                Expanded(child: SizedBox(height: 34, child: TextField(
+                  style: TextStyle(fontSize: 13),
+                  decoration: InputDecoration(
+                    suffixIcon: Icon(Icons.photo_library, color: Colors.green,
                     ),
-                    borderRadius: BorderRadius.circular(30.0)
+                    isDense: true,
+                    hintText: "What's on your mind?",
+                    hintStyle: TextStyle(color: Colors.black54, fontSize: 10),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(50)),
+                      borderSide: BorderSide(
+                        color: Colors.black26,
+                        style: BorderStyle.solid,
+                        width: 1
+                      ),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(50)),
+                      borderSide: BorderSide(
+                        color: Colors.black26,
+                        style: BorderStyle.solid,
+                        width: 1
+                      ),
+                    ),
                   ),
-                  child: const Text('Bạn đang nghĩ gì?'),
                 ),
-
-                const SizedBox(width: 10.0),
-                Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 5.0, vertical: 10.0),
-                    child: const Icon(Icons.photo_library, size: 20.0, color: Colors.green),
-                )
-
+                ),
+                ),
               ],
             ),
           ),
-
-          // const Divider(),
-
-          // Container(
-          //   margin: const EdgeInsets.symmetric(vertical: 10.0),
-          //   child: Row(
-          //     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          //     crossAxisAlignment: CrossAxisAlignment.center,
-          //     children: <Widget>[
-          //       Row(
-          //         children: <Widget>[
-          //           const Icon(Icons.live_tv, size: 20.0, color: Colors.pink,),
-          //           const SizedBox(width: 5.0,),
-          //           Text('Live', style: TextStyle(color: Colors.grey[600], fontWeight: FontWeight.bold, fontSize: 16.0)),
-          //         ],
-          //       ),
-          //       Container(height: 20, child: VerticalDivider(color: Colors.grey[600])),
-          //       Row(
-          //         children: <Widget>[
-          //           const Icon(Icons.photo_library, size: 20.0, color: Colors.green,),
-          //           const SizedBox(width: 5.0),
-          //           Text('Photo', style: TextStyle(color: Colors.grey[600], fontWeight: FontWeight.bold, fontSize: 16.0)),
-          //         ],
-          //       ),
-          //       SizedBox(height: 20, child: VerticalDivider(color: Colors.grey[600])),
-          //       Row(
-          //         children: <Widget>[
-          //           const Icon(Icons.video_call, size: 20.0, color: Colors.purple,),
-          //           const SizedBox(width: 5.0,),
-          //           Text('Room', style: TextStyle(color: Colors.grey[600], fontWeight: FontWeight.bold, fontSize: 16.0)),
-          //         ],
-          //       )
-          //     ],
-          //   ),
-          // )
         ],
       ),
     );
